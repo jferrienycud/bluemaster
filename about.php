636c137fe17fa9fef64d8c564651ef036a07b97f
John Ferrie <jferrienycud@gmail.com>
2011-07-21 18:46:28 +0000

    added structure for home page

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php include('includes/header.php')?>
+  <div class="content">
+    <div class="main-col">
+      
+    </div>
+    <div class="alt-col">
+      
+    </div>
+  </div>
+<?php include('includes/footer.php')?>
